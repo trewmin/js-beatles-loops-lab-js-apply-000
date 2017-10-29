6369be8731bc6bf4b50a5886b8b99b75927b932d
Truman Lahr <trumanlahr@gmail.com>
2017-10-29 00:03:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var theBeatlesPlay = function(musicians,instruments) {
   var stringBeatles = []
   var lenMus= musicians.length
   var lenIns= instruments.length
-  for (i=0; i < lenMus; i++) {
+  for (var i=0; i < lenMus; i++) {
     x = muscians[i]
-    for (j=0; j < lenIns; j++) {
+    for (var j=0; j < lenIns; j++) {
       y = instruments[j]
     }
     stringBeatles.push( x + "plays" + y)
