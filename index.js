a3edbb830ba931dc534f53a7e3f3546184b79755
Truman Lahr <trumanlahr@gmail.com>
2017-10-29 00:18:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians,instruments) {
   var arrayBeatles = []
   for (let i=0; i < musicians.length; i++) {
-  arrayBeatles.push(musicians[i] + " plays " instruments[i])
+  arrayBeatles.push(musicians[i] + " plays " + instruments[i])
   }
   return arrayBeatles
 }
