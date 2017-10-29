1e047bba17fe1b829023201ad6a11ce61619c54c
Truman Lahr <trumanlahr@gmail.com>
2017-10-29 00:17:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 function theBeatlesPlay(musicians,instruments) {
   var arrayBeatles = []
   for (let i=0; i < musicians.length; i++) {
-    var x = musicians[i]
-    for (let j=0; j < instruments.length; j++) {
-      var y = instruments[j]
-    }
-  var stringBeatles = x + 'plays' + y
-  arrayBeatles.push(stringBeatles)
+  arrayBeatles.push(musicians[i] + " plays " instruments[i])
   }
   return arrayBeatles
 }
